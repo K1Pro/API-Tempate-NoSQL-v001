20007df0831eeafaa6079a4feaefedccbe35918c
K1Pro <support@k1pro.net>
2023-09-19 14:59:55 +0200

    adding emits to login comoponent

--- frontend/src/App_vue.js
+++ frontend/src/App_vue.js
@@ -3,12 +3,131 @@ import Login from './components/Login_vue.js';
 
 export default {
   name: 'App',
+  template: /*html*/ `
+    <snackbar></snackbar>
+    <template v-if="accessToken === undefined">
+      <login @access-token-change="updateAccessToken"></login>
+    </template>
+    <template v-else>
+      <button :style="logoutBtn" type="button" @click="logoutFunc(logoutEndPt)">Log Out</button>
+      <div class="grid-container">
+        <div class="item1">
+          <p>
+            Hi <b>{{ userData.FirstName }}</b>,
+          </p>
+          <p>Below is your account info:</p>
+          <ul>
+            <li v-for="(value, key) in userData">{{ key }}: {{ value }}</li>
+          </ul>
+          <p></p>
+        </div>
+        <div class="item2">User Accounts</div>
+        <div class="item3">ProSign</div>
+        <div class="item4">Email</div>
+        <div class="item5">Social Media</div>
+      </div>
+    </template>
+  `,
+
   components: {
     Login,
   },
-  template: /*html*/ `
-    <snackbar></snackbar>
-    <login></login>
-  `,
+  data() {
+    return {
+      accessToken: this.getCookie('_a_t'),
+      sessionID: this.getCookie('_s_i'),
+      userData: '',
+    };
+  },
+  methods: {
+    snackbar(message) {
+      const snackbar = document.getElementById('snackbar');
+      snackbar.innerHTML = message;
+      snackbar.className = 'show';
+      setTimeout(function () {
+        snackbar.className = snackbar.className.replace('show', '');
+      }, 3000);
+    },
+
+    getCookie(name) {
+      return document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))?.at(2);
+    },
+
+    updateAccessToken(accessToken, sessionID) {
+      this.accessToken = accessToken;
+      this.sessionID = sessionID;
+    },
+
+    async logoutFunc(endPt) {
+      try {
+        const response = await fetch(servrURL + endPt + this.sessionID, {
+          method: 'DELETE',
+          headers: {
+            Authorization: this.accessToken,
+            'Cache-Control': 'no-store',
+          },
+        });
+        const logOutResJSON = await response.json();
+        if (logOutResJSON.success) {
+          this.userData = '';
+          this.sessionID = '';
+          this.accessToken = undefined;
+          document.cookie = `_a_t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
+          document.cookie = `_s_i=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
+        }
+        this.snackbar(logOutResJSON.messages[0]);
+      } catch (error) {
+        this.error = error.toString();
+        this.snackbar(this.error);
+      }
+    },
+
+    async userDataFunc(endPt) {
+      try {
+        const response = await fetch(servrURL + endPt, {
+          method: 'GET',
+          headers: {
+            Authorization: this.accessToken,
+            'Cache-Control': 'no-store',
+          },
+        });
+        const userDataResJSON = await response.json();
+        if (userDataResJSON.success) {
+          this.userData = userDataResJSON.data.user;
+        } else {
+          document.cookie = `_a_t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
+          document.cookie = `_s_i=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
+          this.accessToken = undefined;
+        }
+      } catch (error) {
+        this.error = error.toString();
+        this.snackbar(this.error);
+      }
+    },
+  },
+  computed: {},
+
+  watch: {
+    accessToken(newToken, oldToken) {
+      console.log('this has changed');
+      if (newToken != undefined) this.userDataFunc(this.userDataEndPt);
+    },
+  },
+
+  created() {
+    this.logoutEndPt = 'controller/sessions.php?sessionid=';
+    this.userDataEndPt = 'controller/users.php?userid=';
+    if (this.accessToken) {
+      this.userDataFunc(this.userDataEndPt);
+    }
+
+    // <style scoped>
+    this.logoutBtn = {
+      position: 'absolute',
+      top: '5px',
+      right: '15px',
+    };
+    // </style>
+  },
 };
 // </script>
--- frontend/src/components/Login_vue.js
+++ frontend/src/components/Login_vue.js
@@ -2,30 +2,33 @@
 export default {
   name: 'Login',
 
+  template: /*html*/ `
+    <div class="content">
+      <div class="loginform">
+        <div class="square"></div>
+        <div class="inputs">
+          <label>Username: </label>
+          <input type="text" v-model="username" /><br /><br />
+          <label>Password: </label>
+          <input type="password" v-model="password" /><br /><br />
+          <button type="button" @click="loginFunc(loginEndPt)">Log In</button>
+        </div>
+      </div>
+    </div>
+  `,
+
+  // props: ['accessToken', 'sessionID'],
+
   data() {
     return {
       username: '',
       password: '',
-      accessToken: this.getCookie('_a_t'),
-      sessionID: this.getCookie('_s_i'),
-      userData: '',
     };
   },
 
+  emits: ['access-token-change'],
+
   methods: {
-    snackbar(message) {
-      const snackbar = document.getElementById('snackbar');
-      snackbar.innerHTML = message;
-      snackbar.className = 'show';
-      setTimeout(function () {
-        snackbar.className = snackbar.className.replace('show', '');
-      }, 3000);
-    },
-
-    getCookie(name) {
-      return document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))?.at(2);
-    },
-
     async loginFunc(endPt) {
       try {
         const response = await fetch(servrURL + endPt, {
@@ -42,8 +45,11 @@ export default {
         });
         const logInResJSON = await response.json();
         if (logInResJSON.success) {
-          this.accessToken = logInResJSON.data.accesstoken;
-          this.sessionID = logInResJSON.data.session_id;
+          this.$emit(
+            'access-token-change',
+            logInResJSON.data.accesstoken,
+            logInResJSON.data.session_id
+          );
           const tomorrow = new Date();
           tomorrow.setDate(tomorrow.getDate() + 1);
           document.cookie = `_a_t=${
@@ -53,121 +59,18 @@ export default {
             logInResJSON.data.session_id
           }; expires=${tomorrow.toString()};`;
         }
-        this.snackbar(logInResJSON.messages[0]);
+        // this.snackbar(logInResJSON.messages[0]);
       } catch (error) {
         this.error = error.toString();
-        this.snackbar(this.error);
+        // this.snackbar(this.error);
       }
-    },
-
-    async logoutFunc(endPt) {
-      try {
-        const response = await fetch(servrURL + endPt + this.sessionID, {
-          method: 'DELETE',
-          headers: {
-            Authorization: this.accessToken,
-            'Cache-Control': 'no-store',
-          },
-        });
-        const logOutResJSON = await response.json();
-        if (logOutResJSON.success) {
-          this.userData = '';
-          this.sessionID = '';
-          this.accessToken = undefined;
-          document.cookie = `_a_t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
-          document.cookie = `_s_i=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
-        }
-        this.snackbar(logOutResJSON.messages[0]);
-      } catch (error) {
-        this.error = error.toString();
-        this.snackbar(this.error);
-      }
-    },
-
-    async userDataFunc(endPt) {
-      try {
-        const response = await fetch(servrURL + endPt, {
-          method: 'GET',
-          headers: {
-            Authorization: this.accessToken,
-            'Cache-Control': 'no-store',
-          },
-        });
-        const userDataResJSON = await response.json();
-        if (userDataResJSON.success) {
-          this.userData = userDataResJSON.data.user;
-        } else {
-          document.cookie = `_a_t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
-          document.cookie = `_s_i=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=${cookiePath};`;
-          this.accessToken = undefined;
-        }
-      } catch (error) {
-        this.error = error.toString();
-        this.snackbar(this.error);
-      }
-    },
-  },
-  computed: {},
-
-  watch: {
-    accessToken(newToken, oldToken) {
-      if (newToken != undefined) this.userDataFunc(this.userDataEndPt);
     },
   },
 
   created() {
     this.loginEndPt = 'controller/sessions.php';
-    this.logoutEndPt = 'controller/sessions.php?sessionid=';
-    this.userDataEndPt = 'controller/users.php?userid=';
-    if (this.accessToken) {
-      this.userDataFunc(this.userDataEndPt);
-    }
-
-    // <style scoped>
-    this.logoutBtn = {
-      position: 'absolute',
-      top: '5px',
-      right: '15px',
-    };
-    // </style>
   },
 
   beforeMount() {},
-
-  template: /*html*/ `
-  <template v-if="accessToken === undefined">
-    <div class="content">
-      <div class="loginform">
-        <div class="square"></div>
-        <div class="inputs">
-          <label>Username: </label>
-          <input type="text" v-model="username" /><br /><br />
-          <label>Password: </label>
-          <input type="password" v-model="password" /><br /><br />
-          <button type="button" @click="loginFunc(loginEndPt)">Log In</button>
-        </div>
-      </div>
-    </div>
-  </template>
-  <template v-else>
-    <button :style="logoutBtn" type="button" @click="logoutFunc(logoutEndPt)">Log Out</button>
-    <div class="grid-container">
-      <div class="item1">
-        <p>
-          Hi <b>{{ userData.FirstName }}</b>,
-        </p>
-        <p>Below is your account info:</p>
-        <ul>
-          <li v-for="(value, key) in userData">{{ key }}: {{ value }}</li>
-        </ul>
-        <p></p>
-      </div>
-      <div class="item2">User Accounts</div>
-      <div class="item3">ProSign</div>
-      <div class="item4">Email</div>
-      <div class="item5">Social Media</div>
-    </div>
-  </template>
-`,
 };
 // </script>
