7690c8970bfc6d1b638f0a829743c89fd5c666b4
K1Pro <support@k1pro.net>
2023-09-21 19:58:54 +0200

    polishing up design of social media module

--- frontend/src/components/SocialMedia_vue.js
+++ frontend/src/components/SocialMedia_vue.js
@@ -3,14 +3,16 @@ export default {
   name: 'SocialMedia',
 
   template: /*html*/ `
-    <span>Social Media</span><br><br>
-    <input type="search" v-model="imageSearchInput" name="image-search" placeholder="Search for an image…"/><br>
-    <button type="button" @click.prevent="imageSearch()">Search</button>
+    <b>Social Media</b><br><br>
+    <input type="search" v-model="imageSearchInput" name="image-search" placeholder="Search for an image…"/>
+    <button type="button" @click.prevent="imageSearch()">Search</button><br>
+    <img v-if="randomImagePath" :src="randomImagePath" alt="random-image">
   `,
 
   data() {
     return {
       imageSearchInput: '',
+      randomImagePath: '',
     };
   },
 
@@ -30,7 +32,17 @@ export default {
         );
         const imageSearchJSON = await response.json();
         if (imageSearchJSON) {
-          console.log(imageSearchJSON.total_results);
+          console.log('total results' + imageSearchJSON.total_results);
+          const max =
+            imageSearchJSON.total_results > 80
+              ? 80
+              : imageSearchJSON.total_results;
+          const randomImage = Math.floor(Math.random() * (max - 0 + 1) + 0);
+          console.log('random number' + randomImage);
+          console.log(imageSearchJSON.photos);
+          console.log(imageSearchJSON.photos[randomImage].src['landscape']);
+          this.randomImagePath =
+            imageSearchJSON.photos[randomImage].src['landscape'];
         }
       } catch (error) {
         console.log(error.toString());
